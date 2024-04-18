0e9d4e1a0d8b1da635a421d6b417108f184e0cc2
Dorcas Chagas <128332474+Dorcaschagas@users.noreply.github.com>
2024-04-18 09:40:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-$(window).scroll(function () {
-    var scrollVertical = window.scrollY || window.pageYOffset;
-    if (scrollVertical === 0) {
-        $("#scrollTop").addClass('d-none')
-    } else {
-        $("#scrollTop").removeClass('d-none')
-    }
+//===================================================
+//leva a tela ao inicio
+$(window).on('scroll',function(){
+    $("#scrollTop").toggleClass('d-none', $(this).scrollTop() <= 0);
 })
 
-function scrollTopBtn() {
-    $(window).scrollTop(0)
-    $('html , body').animate({
-        scrollTop: 0
-    }, 'slow')
-}
+$("#scrollTop").on('click', function(){
+   $('html, body').scrollTop(0)
+})
+//===================================================
+//permite mover botao scroll na tela.
+let movendoMouse = false;
+let inicioXMouse;
+
+$('.scrollmoving').on('mousedown', function (e) {
+    movendoMouse = true;
+    inicioXMouse = e.clientX - parseInt($('#scrollTop').css('left'));
+});
+
+$(document).on('mousemove', function (e) {
+    if (movendoMouse) {
+        let newLeft = e.clientX - inicioXMouse;
+        $('.scrollmoving').css('left', newLeft);
+    }
+});
+
+$(document).on('mouseup', function () {
+    movendoMouse = false;
+});
